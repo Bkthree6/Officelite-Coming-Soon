e4b8ef1c0b1dbe8a34c4a9a01629fc906a5861d8
Brandon <bkthree56@gmail.com>
2021-03-24 18:59:32 -0500

    Finished dynamically changing the coming date.

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,2 +1,32 @@
-let date = document.getElementById("date");
-console.log(date);
+const numToMonthConv = {
+    jan: "JAN",
+    feb: "FEB",
+    mar: "MAR",
+    apr: "APR",
+    may: "MAY",
+    jun: "JUN",
+    jul: "JUL",
+    aug: "AUG",
+    sep: "SEP",
+    oct: "OCT",
+    nov: "NOV",
+    dec: "DEC"
+}
+
+var date = new Date();
+var futureDate = date.setDate(date.getDate() + 47);
+
+var day = date.getDate();
+
+
+var month = date.getMonth();
+
+
+var year = date.getFullYear();
+
+
+var stringMonth = Object.values(numToMonthConv)[month];
+
+
+let returnDate = `${day} ${stringMonth} ${year}`;
+document.getElementById('date').innerHTML= returnDate;
